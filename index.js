75a2ef2870498bcc3db082dad2c9e9d56acf7f75
Sanjh Chhatrasal <sanjh.chhatrasal@shl.com>
2026-01-28 18:07:13 +0530

    adding index file

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const pino = require('pino');
+const pinoLoki = require('pino-loki');
+
+const app = express();
+
+const logger = pino(
+  pinoLoki({
+    host: "http://loki:3100/loki/api/v1/push",
+    interval: 2, 
+    labels: { app: 'status-app' },
+  })
+);
+
+function getRandomStatus() {
+  const statuses = [200, 201, 202, 400, 401, 403, 404, 500, 502, 503];
+  const randomIndex = Math.floor(Math.random() * statuses.length);
+  return statuses[randomIndex];
+}
+
+
+app.get('/', (req, res) => {
+  const status = getRandomStatus();
+  logger.info({ status }, 'Random status returned'); // Log to Loki
+  res.status(status).send(`Status code: ${status}`);
+});
+
+// Start server
+const port = 8877;
+app.listen(port, () => {
+  logger.info(`Server running on port ${port}`);
+});
