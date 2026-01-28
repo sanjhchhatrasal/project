3e3a191f7bbaa79599f4fee17d6b7cab298f4c90
Sanjh Chhatrasal <sanjh.chhatrasal@shl.com>
2026-01-28 19:31:32 +0530

    adding

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 
 const logger = pino(
   pinoLoki({
-    // host: "https://tcplusdev-otel.shl.zone/loki/api/v1/push",
-    host: "http://10.112.1.113:3100/loki/api/v1/push",
+    host: "http://loki:3100/loki/api/v1/push",
+    // host: "http://10.112.1.113:3100/loki/api/v1/push",
     interval: 2,
     labels: { app: 'status-app' },
   })
