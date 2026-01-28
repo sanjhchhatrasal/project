9b7b7eccacbe60f1f1c36457862764b0d7a776fb
Sanjh Chhatrasal <sanjh.chhatrasal@shl.com>
2026-01-28 18:48:52 +0530

    adding

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const logger = pino(
   pinoLoki({
     host: "http://10.112.1.113:3100/loki/loki/api/v1/push", // include the extra /loki
+    grpc: "http://10.112.1.113:9095/loki/loki/api/v1/push/",
     interval: 2,
     labels: { app: 'status-app' },
   })
